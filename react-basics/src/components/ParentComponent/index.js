27cce2bbf7b08e4bf93d8241f1f05dc61839e462
sangeetha2521 <sangeens8101@gmail.com>
2022-12-23 17:05:58 +0530

    Challenge added

--- react-basics/src/components/ParentComponent/index.js
+++ react-basics/src/components/ParentComponent/index.js
@@ -1,3 +1,5 @@
+// Challenge: Parent text (I need to be updated from my child) should be updated when Child button below is clicked. Feel free to use any string to update the parent's current string.
+
 import react, { useState } from "react";
 import ChildComponent from "./ChildComponent";
 
